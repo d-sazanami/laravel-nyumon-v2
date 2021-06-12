f44af9b234f90246c556149bb23252689cc5734d
t-sagiri <sagirix.t@gmail.com>
2021-06-12 13:20:13 +0900

    シングルアクションコントローラでHelloページ作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,41 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
+class HelloController extends Controller
+{
+    public function __invoke()
+    {
+        return <<<EOF
+<html>
+<head>
+<title>Hello</title>
 <style>
 body { font-size:16pt; color:#999; }
 h1 { font-size:100pt; text-align:right; color:#eee;
     margin:-40px 0px -50px 0px; }
 </style>
+</head>
+<body>
+    <h1>Single Action</h1>
+    <p>これは、シングルアクションコントローラです。</p>
+</body>
+</html>
 EOF;
-$body = '</head><body>';
-$end = '</body></html>';
 
-function tag($tag, $txt) {
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
-
-class HelloController extends Controller
-{
-    public function index($id='noname', $pass='unknown') {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title', 'Hello/Index') . $style . $body
-            . tag('h1', 'Index') . tag('p', 'this is Index page')
-            . '<a href="/hello/oher">go to other page</a>'
-            . $end;
-        return $html;
-    }
-
-    public function other()
-    {
-        global $head, $style, $body, $end;
-        $html = $head . tag('title', 'Hello/Other') . $style . $body
-            . tag('h1', 'Other') . tag('p', 'this is Other page')
-            . $end;
-        return $html;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', 'App\Http\Controllers\HelloController@index');
-Route::get('/hello/oher', 'App\Http\Controllers\HelloController@other');
+Route::get('/hello', HelloController::class);
