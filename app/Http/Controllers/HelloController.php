653b414eacf57dd525c3cf5044fc9c8d75fb9ed5
t-sagiri <sagirix.t@gmail.com>
2021-06-15 09:38:39 +0900

    @issetディレクティブを使って表示を制御するよう変更

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['msg' => '']);
+        return view('hello.index');
     }
 
     public function post(Request $request)
