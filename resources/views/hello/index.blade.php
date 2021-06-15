22f971e5cfe7e9f6cfbe7e7d2f6b091bab6aa1fb
t-sagiri <sagirix.t@gmail.com>
2021-06-15 09:31:16 +0900

    テンプレートのタブをスペースに置換

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,23 +1,23 @@
 <html>
 <head>
-	<title>Hello/Index</title>
-	<style>
-	body {font-size: 16pt; color: #999; }
-	h1 {font-size: 50pt; text-align: right; color: #f6f6f6;
-	margin: -20px 0px -30px 0px; letter-spacing: -4pt; }
-	</style>
+        <title>Hello/Index</title>
+        <style>
+        body {font-size: 16pt; color: #999; }
+        h1 {font-size: 50pt; text-align: right; color: #f6f6f6;
+        margin: -20px 0px -30px 0px; letter-spacing: -4pt; }
+        </style>
 </head>
 <body>
-	<h1>Blade/Index</h1>
-	@if ($msg != '')
-	<p>こんにちは、{{$msg}}さん。</p>
-	@else
-	<p>何か書いて下さい。</p>
-	@endif
-	<form action="/hello" method="post">
-		@csrf
-		<input type="text" name="msg">
-		<input type="submit">
-	</form>
+        <h1>Blade/Index</h1>
+        @if ($msg != '')
+        <p>こんにちは、{{$msg}}さん。</p>
+        @else
+        <p>何か書いて下さい。</p>
+        @endif
+        <form action="/hello" method="post">
+                @csrf
+                <input type="text" name="msg">
+                <input type="submit">
+        </form>
 </body>
 </html>
