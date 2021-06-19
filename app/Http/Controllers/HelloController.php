82b0787c028b58d9d3196cd895f8dfe417821e81
t-sagiri <sagirix.t@gmail.com>
2021-06-19 16:51:16 +0900

    バリデータを使ってみる

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Dotenv\Validator as DotenvValidator;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -13,8 +14,18 @@ class HelloController extends Controller
         return view('hello.index', ['msg'=>'フォームを入力:']);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/hello')
+            ->withErrors($validator)
+            ->withInput();
+        }
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
