e2769f720f4d875683e035b017b77771b5283904
t-sagiri <sagirix.t@gmail.com>
2021-06-22 17:14:05 +0900

    簡単なSデータ取得と表示
    - DBに直接アクセスして作ったテーブルからデータ取得、viewに表示する

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,17 +8,14 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Validator;
 use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie:' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        return view('hello.index', ['msg' => $msg]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
 
     public function post(Request $request)
