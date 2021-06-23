2d6e4ab367f12526d8259f109160d704a8974f4c
t-sagiri <sagirix.t@gmail.com>
2021-06-23 10:59:44 +0900

    peopleへデータを登録する処理作成
    - バリデーションなし、単純insert

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -14,28 +14,32 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if (isset($request->id)) {
-            $param = ['id' => $request->id];
-            $items = DB::select('select * from people where id = :id',
-                $param);
-        } else {
-            $items = DB::select('select * from people');
-        }
-
+        $items = DB::select('select * from people');
         return view('hello.index', ['items' => $items]);
     }
 
     public function post(Request $request)
     {
-        $validate_rule = [
-            'msg' => 'required',
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
+
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
         ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = response()->view('hello.index',
-            ['msg' => '「' . $msg . '」をクッキーに保存しました。']
+        DB::insert(
+            'insert into people (name, mail, age) values (:name, :mail, :age)',
+            $param
         );
-        $response->cookie('msg', $msg, 100);
-        return $response;
+        return redirect('/hello');
     }
 }
