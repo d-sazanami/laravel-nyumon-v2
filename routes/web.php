bc609d78db15b7da4e3eea3b0da3a0eca1bc9a19
t-sagiri <sagirix.t@gmail.com>
2021-06-30 15:02:04 +0900

    restdata用コントローラ生成
    - ついでにルーティングも

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,5 @@ Route::post('person/del', 'App\Http\Controllers\PersonController@remove');
 Route::get('board', 'App\Http\Controllers\BoardController@index');
 Route::get('board/add', 'App\Http\Controllers\BoardController@add');
 Route::post('board/add', 'App\Http\Controllers\BoardController@create');
+
+Route::resource('rest', 'RestappController');
