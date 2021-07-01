157bf6cd6dee0a9cce258f1409511cc5c1c4f80a
t-sagiri <sagirix.t@gmail.com>
2021-07-01 13:19:22 +0900

    restdata新規登録処理作成

--- app/Http/Controllers/RestappController.php
+++ app/Http/Controllers/RestappController.php
@@ -36,7 +36,11 @@ class RestappController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $restdata = new Restdata();
+        $form = $request->all();
+        unset($form['_token']);
+        $restdata->fill($form)->save();
+        return redirect('rest');
     }
 
     /**
